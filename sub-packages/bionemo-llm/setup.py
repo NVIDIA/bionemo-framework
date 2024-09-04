93117fdd973bfdd474490bdc4aa1aa7dce2c726f
Malcolm Greaves <mgreaves@nvidia.com>
2024-09-04 15:03:44 -0400

    BIONEMO_PUBLISH_MODE pin version; otherwise use path dependent

--- sub-packages/bionemo-llm/setup.py
+++ sub-packages/bionemo-llm/setup.py
@@ -12,31 +12,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-from setuptools import setup
-
-
+import os
 from pathlib import Path
+
 from setuptools import setup
 
 
-_version_file = Path(__file__).absolute().parent.parent.parent / 'VERSION'
-if not _version_file.is_file():
-    raise ValueError(f"ERROR: cannot find VERSION file! {str(_version_file)}")
-with open(str(_version_file), 'rt') as rt:
-    BIONEMO_VERSION: str = rt.read().strip()
-if len(BIONEMO_VERSION) == 0:
-    raise ValueError(f"ERROR: no verison specified in VERSION file! {str(_version_file)}")
+def is_true(x) -> bool:
+    if isinstance(x, str):
+        x = x.strip().lower()
+        return x == "true" or x == "yes" or x == "y"
+    else:
+        return x is True
 
-_reqs_file = Path(__file__).absolute().parent / 'requirements.txt'
-if not _reqs_file.is_file():
-    raise ValueError(f"ERROR: no requirements.txt file present! {str(_reqs_file)}")
+
+BIONEMO_PUBLISH_MODE: bool = is_true(os.environ.get("BIONEMO_PUBLISH_MODE"))
 
 
 def read_reqs(f: str) -> list[str]:
     lines = []
-    with open(f, 'rt') as rt:
+    with open(f, "rt") as rt:
         for l in rt:
             l = l.strip()
             if len(l) == 0 or l.startswith("#"):
@@ -46,31 +41,37 @@ def read_reqs(f: str) -> list[str]:
 
 
 LOCAL_REQS: list[str] = [
-    'bionemo-core',
+    "bionemo-core",
 ]
 
 
 if __name__ == "__main__":
-    # L = dict(**locals())
-    # G = dict(**globals())
-    #
-    # def write(wt, mapping):
-    #     for k, v in mapping.items():
-    #         wt.write(f"\t{k} ({type(k)}):  ({type(v)}) {v}\n")
-    #         wt.write('-'*80)
-    #         wt.write("\n")
-    #
-    # with open('here.txt', 'wt') as wt:
-    #     wt.write("globals()=\n")
-    #     write(wt, G)
-    #
-    #     wt.write("\n\nlocals()=\n")
-    #     write(wt, L)
-    # import ipdb
-    #
-    # ipdb.set_trace()
+    repo_root = Path(__file__).absolute().parent.parent.parent
+
+    _version_file = repo_root / "VERSION"
+    if not _version_file.is_file():
+        raise ValueError(f"ERROR: cannot find VERSION file! {str(_version_file)}")
+    with open(str(_version_file), "rt") as rt:
+        BIONEMO_VERSION: str = rt.read().strip()
+    if len(BIONEMO_VERSION) == 0:
+        raise ValueError(f"ERROR: no version specified in VERSION file! {str(_version_file)}")
+
+    _reqs_file = Path(__file__).absolute().parent / "requirements.txt"
+    if not _reqs_file.is_file():
+        raise ValueError(f"ERROR: no requirements.txt file present! {str(_reqs_file)}")
+
+    reqs = read_reqs(str(_reqs_file))
+    if BIONEMO_PUBLISH_MODE:
+        for x in LOCAL_REQS:
+            reqs.append(f"{x}=={BIONEMO_VERSION}")
+    else:
+        for x in LOCAL_REQS:
+            sub_package_root = repo_root / "sub-packages" / x
+            if not sub_package_root.is_dir():
+                raise ValueError(f'ERROR: sub-package "{x}" does not exist locally! ({sub_package_root})')
+            reqs.append(f"{x} @ {str(sub_package_root)}")
 
     setup(
         version=BIONEMO_VERSION,
-        install_requires=LOCAL_REQS + read_reqs(str(_reqs_file)),
+        install_requires=reqs,
     )
