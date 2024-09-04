293555d40c5ba6d92ae7b953a7b37d0fa42e6ac2
Malcolm Greaves <mgreaves@nvidia.com>
2024-09-04 15:19:03 -0400

    popigate setup.py and pyproject.toml structural changes (wip)

--- sub-packages/bionemo-core/setup.py
+++ sub-packages/bionemo-core/setup.py
@@ -12,10 +12,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from pathlib import Path
 
 from setuptools import setup
 
 
+# **NO** LOCAL_REQS allowed for bionemo-core !!!
+
+# Therefore, no need for BIONEMO_PUBLISH_MODE
+
+# Therefore, we can use the requirements.txt file as-is for dependencies.
+# Meaning we can just fill it in dynamically from the pyproject.toml !!!
+
 if __name__ == "__main__":
-    setup()
+    repo_root = Path(__file__).absolute().parent.parent.parent
+
+    _version_file = repo_root / "VERSION"
+    if not _version_file.is_file():
+        raise ValueError(f"ERROR: cannot find VERSION file! {str(_version_file)}")
+    with open(str(_version_file), "rt") as rt:
+        BIONEMO_VERSION: str = rt.read().strip()
+    if len(BIONEMO_VERSION) == 0:
+        raise ValueError(f"ERROR: no version specified in VERSION file! {str(_version_file)}")
+
+    setup(
+        version=BIONEMO_VERSION,
+    )
