28f4d48a833450ed8bf2989f2474d4b34de3f92e
Malcolm Greaves <mgreaves@nvidia.com>
2024-09-05 16:40:24 -0400

    ruff check fixes

--- sub-packages/bionemo-fw/setup.py
+++ sub-packages/bionemo-fw/setup.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ruff: noqa: D101, D102, D103, D107
 import os
 from pathlib import Path
 from typing import Optional
