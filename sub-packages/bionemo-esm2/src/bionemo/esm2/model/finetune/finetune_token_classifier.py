003e9202742b378b13d598f035597e3574e290c0
Farhad Ramezanghorbani <farhad.ghorbani@gmail.com>
2024-09-20 19:38:47 +0000

    move label tokenizer

--- sub-packages/bionemo-esm2/src/bionemo/esm2/model/finetune/finetune_token_classifier.py
+++ sub-packages/bionemo-esm2/src/bionemo/esm2/model/finetune/finetune_token_classifier.py
@@ -15,18 +15,18 @@
 
 
 from dataclasses import dataclass, field
-from typing import Dict, Iterable, List, Sequence, Tuple, Type, Union
+from typing import Dict, List, Sequence, Tuple, Type, Union
 
 import torch
 from megatron.core import parallel_state
 from megatron.core.transformer.module import MegatronModule
 from megatron.core.transformer.transformer_config import TransformerConfig
-from nemo.collections.common.tokenizers import TokenizerSpec
 from nemo.lightning.megatron_parallel import MegatronLossReduction, ReductionT
 from torch.utils.data import Dataset
 
 from bionemo.esm2.api import ESM2GenericConfig, ESM2Model
 from bionemo.esm2.data import tokenizer
+from bionemo.llm.data.label2id_tokenizer import Label2IDTokenizer
 from bionemo.llm.model.loss import BERTMLMLossWithReduction, PerTokenLossDict, SameSizeLossDict
 from bionemo.llm.utils import iomixin_utils as iom
 
@@ -167,108 +167,6 @@ class ESM2FineTuneSeqLenBioBertConfig(ESM2GenericConfig[ESM2FineTuneSeqLengthMod
 
     def get_loss_reduction_class(self) -> Type[MegatronLossReduction]:
         return ClassifierLossReduction
-
-
-class Label2IDTokenizer(TokenizerSpec):
-    """Char Tokenizer for sequence labels.
-
-    Intended to be used for extracting class labels for classification models such as secondary structure prediction
-    model, where each class is encoded with a character (ex. "C", "H", "E").
-
-    Examples:
-        >>> tokenizer = Label2IDTokenizer()
-        >>> seqs = ['CHE', 'CCC', 'EHH']
-        >>> tokenizer = tokenizer.build_vocab(s)
-    """
-
-    def __init__(self):
-        self.vocab = {}
-        self._update_index()
-
-    def _update_index(self):
-        """Updates the id_to_vocab index based on the current vocab."""
-        self.decode_vocab = {id_: token for token, id_ in self.vocab.items()}
-
-    @property
-    def vocab_size(self) -> int:
-        """Return the size of the vocab being used."""
-        return len(self.vocab)
-
-    def text_to_tokens(self, text: str) -> List[str]:
-        return list(text)
-
-    def tokens_to_text(self, tokens):
-        return "".join(tokens)
-
-    def tokens_to_ids(self, tokens: List[str]) -> List[int]:
-        """Convert tokens to indexes/ids.
-
-        Args:
-            tokens (List[str]):  Containing tokens
-        Returns:
-            (List[int]): Containing ID's for each token
-        """
-        ids = []
-        for token in tokens:
-            id_ = self.vocab.get(token)
-            if id_ is None:
-                raise ValueError(f"Do not recognize token: {token}")
-            else:
-                ids.append(id_)
-        return ids
-
-    def ids_to_tokens(self, ids: List[int]) -> List[str]:
-        """Convert Ids to tokens.
-
-        Args:
-            ids (List[int]): Contains ids for each token
-        Returns:
-            (List[str]): Contains tokens
-        """
-        tokens = []
-        for id_ in ids:
-            token = self.decode_vocab.get(id_)
-            if token is None:
-                raise ValueError(f"Do not recognize ID: {id_}")
-            tokens.append(token)
-        return tokens
-
-    def text_to_ids(self, text: str) -> List[int]:
-        """Converts text to ids.
-
-        Args:
-            text (str): String containing text to convert
-        Returns:
-            (List[int]): Id's corresponding to the tokenization
-            of the text
-        """
-        tokens = self.text_to_tokens(text)
-        return self.tokens_to_ids(tokens)
-
-    def ids_to_text(self, ids):
-        tokens = self.ids_to_tokens(ids)
-        return self.tokens_to_text(tokens)
-
-    def build_vocab(self, strings: Union[str, Iterable[str]]):
-        """Builds the vocabulary of the tokenizer from strings.
-
-        Args:
-            strings: (Union[str, Iterable[str]]): Strings to
-                build the vocabulary with. If a string is supplied,
-                then the vocabulary is built from the single string.
-                Otherwise, the vocabulary is progressively built
-                from all of the strings in `strings`.
-        """
-        if isinstance(strings, str):
-            strings = [strings]
-
-        for string in strings:
-            for token in string:
-                if token not in self.vocab:
-                    self.vocab[token] = len(self.vocab)
-                    self.decode_vocab[self.vocab[token]] = token
-
-        return self
 
 
 class PerTokenValueDataset(Dataset):
