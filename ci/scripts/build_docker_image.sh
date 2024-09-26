a1079555c0353d05510f0db0654b0ca27809b85d
Dorota Toczydlowska <dorotat@nvidia.com>
2024-09-26 18:29:46 +0200

    added version and stable cache tag

--- ci/scripts/build_docker_image.sh
+++ ci/scripts/build_docker_image.sh
@@ -9,13 +9,25 @@ Options:
   -container-registry-path <path>   Path to Docker container registry. Used for image name and cache retrieval if -use-cache is enabled.
   -dockerfile-path <path>           Optional. Path to the Dockerfile. Default: setup/Dockerfile.
   -use-cache                        Enable Docker image caching for faster builds.
-  -image-tag <string>               Optional. Image tag in the format CONTAINER_REGISTRY_PATH:IMAGE_TAG. Default: <GIT_BRANCH_NAME>--<GIT_COMMIT_SHA>.
+  -image-tag <string>               Optional. Custom image tag in the format CONTAINER_REGISTRY_PATH:IMAGE_TAG. Default: <GIT_BRANCH_NAME>--<GIT_COMMIT_SHA>.
   -push                             Push the built Docker image to the registry.
   -print-image-name                 Print only the image name associated with the repository state.
   -cache-args <string>              Optional. Custom cache arguments for building the image.
   -label-args <string>              Optional. Custom label arguments for the Docker image.
   -set-secret                       Optional. Set Docker build secret during image construction. Requires SECRET_VAR_NAME and SECRET_VAR_VALUE to be set.
+  -nightly-cache                    Optional. Use bionemo1--nightly docker image as cache tag of BioNeMo FW to build docker image from. Dy default using the latest released docker image.
   -help                             Display this help message.
+
+Examples:
+  To build a Docker image using caching and push it to the container registry:
+    ./ci/scripts/build_docker_image.sh -container-registry-path <CONTAINER_REGISTRY_PATH> -use-cache -push
+
+  To build and tag a docker image with a custom image tag:
+    ./ci/scripts/build_docker_image.sh --container-registry-path <CONTAINER_REGISTRY_PATH> -image-tag <IMAGE_TAG>
+
+  To print only the default docker image name specific to the repository state:
+    ./ci/scripts/build_docker_image.sh -container-registry-path <CONTAINER_REGISTRY_PATH> -print-image-name
+
 EOF
     exit 1
 }
@@ -38,6 +50,7 @@ while [[ "$#" -gt 0 ]]; do
         -container-registry-path) CONTAINER_REGISTRY_PATH="$2"; shift 2 ;;
         -dockerfile-path) DOCKERFILE_PATH="$2"; shift 2 ;;
         -use-cache) USE_CACHE=true; shift ;;
+        -nightly-cache) USE_NIGHTLY_CACHE=true; shift ;;
         -image-tag) IMAGE_TAG="$2"; shift 2 ;;
         -cache-args) CACHE_ARGS="$2"; shift 2 ;;
         -label-args) LABELS_ARGS="$2"; shift 2 ;;
@@ -82,10 +95,15 @@ DOCKERFILE_PATH="${DOCKERFILE_PATH:-setup/Dockerfile}"
 # Set cache arguments if USE_CACHE is enabled
 if [ "$USE_CACHE" = true ]; then
     if [ -z "$CACHE_ARGS" ]; then
-        IMAGE_TAG_NIGHTLY="bionemo1--nightly"
+        if [ "$USE_NIGHTLY_CACHE" = true ]; then
+          IMAGE_TAG_BIONEMO_CACHE="bionemo1--nightly"
+        else
+          BIONEMO_VERSION=$(awk '{gsub(/^[[:space:]]+|[[:space:]]+$/, ""); printf "%s", $0}' ./VERSION)
+          IMAGE_TAG_BIONEMO_CACHE="${BIONEMO_VERSION}"
+        fi
         CONTAINER_REGISTRY_PATH_NGC="nvcr.io/nvidia/clara/bionemo-framework"
         IMAGE_NAME_CACHE="${CONTAINER_REGISTRY_PATH}:${IMAGE_TAG}--cache"
-        CACHE_ARGS="--cache-from=type=registry,ref=${CONTAINER_REGISTRY_PATH_NGC}:${IMAGE_TAG_NIGHTLY} \
+        CACHE_ARGS="--cache-from=type=registry,ref=${CONTAINER_REGISTRY_PATH_NGC}:${IMAGE_TAG_BIONEMO_CACHE} \
                     --cache-from=type=registry,ref=${IMAGE_NAME_CACHE} \
                     --cache-from=type=registry,ref=${IMAGE_NAME} \
                     --cache-to=type=registry,mode=max,image-manifest=true,ref=${IMAGE_NAME_CACHE}"
