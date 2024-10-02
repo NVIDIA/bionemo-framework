efc19028534f42ca7266379606394c224c6dffdc
Dorota Toczydlowska <dorotat@nvidia.com>
2024-10-02 14:27:31 +0200

    remove xfails since notebook has been fixed

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-def pytest_runtest_setup(item):
-    """Setup of pytest."""
-    if "sub-packages/bionemo-scdl/examples/example_notebook.ipynb" in item.nodeid:
-        pytest.xfail(reason="Issue #228")
