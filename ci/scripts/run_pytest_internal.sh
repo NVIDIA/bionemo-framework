bfb665fb873d6e103ede1563e7958615730d196f
Dorota Toczydlowska <dorotat@nvidia.com>
2024-10-02 13:14:12 +0200

    fixed internal and commented out pytest fork

--- ci/scripts/run_pytest_internal.sh
+++ ci/scripts/run_pytest_internal.sh
@@ -19,4 +19,4 @@ if ! set_bionemo_home; then
     exit 1
 fi
 
-pytest -m "not internal and not needs_fork and not needs_80gb_memory_gpu" -vv --durations=0 --cov=bionemo --cov-report term --cov-report xml:coverage-pytest.xml -k "not test_model_training" -s
+pytest -m "internal" -vv --durations=0 --cov=bionemo --cov-report term --cov-report xml:coverage-internal.xml -k "not test_model_training"
