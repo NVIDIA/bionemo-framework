23b2b252b8ba09787842c992f54798fd7689db2f
Malcolm Greaves <mgreaves@nvidia.com>
2024-10-03 13:30:35 -0700

    wip

---
+++ internal/scripts/check_docker_version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+version_ge() {
+    # Returns 0 (true) if $1 >= $2, 1 (false) otherwise
+    [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" = "$2" ]
+}
+
+docker_version=$(docker --version | awk -F'[, ]' '{print $3}')
+required_docker_version='23.0.1'
+
+if ! version_ge "$docker_version" "$required_docker_version"; then
+    echo "Error: Docker version $required_docker_version or higher is required. Current version: $docker_version"
+    exit 1
+fi
