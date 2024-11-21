79007088b8f2931d659521116717e5bafa79bf54
Farhad Ramezanghorbani <farhad.ghorbani@gmail.com>
2024-11-21 22:17:03 +0000

    add include_input_ids option

--- sub-packages/bionemo-llm/src/bionemo/llm/model/biobert/model.py
+++ sub-packages/bionemo-llm/src/bionemo/llm/model/biobert/model.py
@@ -164,6 +164,7 @@ class MegatronBioBertModel(LanguageModule):
         include_embeddings: bool = False,
         use_full_attention_mask: bool = False,
         include_hiddens: bool = False,
+        include_input_ids: bool = False,
         skip_logits: bool = False,  # Useful for inference time.
     ):
         # TODO (@jstjohn) come up with a cleaner way for this model to return a set of things the user wants.
@@ -195,6 +196,7 @@ class MegatronBioBertModel(LanguageModule):
         self.return_embeddings = return_embeddings
         self.include_embeddings = include_embeddings
         self.include_hiddens = include_hiddens
+        self.include_input_ids = include_input_ids
         self.skip_logits = skip_logits
 
         # megatron core pipelining currently depends on model type
@@ -453,7 +455,7 @@ class MegatronBioBertModel(LanguageModule):
         output = {"token_logits": logits, "binary_logits": binary_logits}
         if self.include_hiddens:
             output["hidden_states"] = hidden_states.transpose(0, 1).contiguous()  # [s b h] => [b s h]
-        if self.include_hiddens or not self.skip_logits:
+        if self.include_input_ids:
             output["input_ids"] = input_ids
         if self.include_embeddings:
             output["embeddings"] = output_embeddings
@@ -517,6 +519,7 @@ class BioBertConfig(
     include_embeddings: bool = False
     return_only_hidden_states: bool = False
     include_hiddens: bool = False  # Include hidden layers in the output of the model
+    include_input_ids: bool = False
     skip_logits: bool = False  # useful for inference
     core_attention_override: Type[torch.nn.Module] | None = None
 
@@ -567,6 +570,7 @@ class BioBertConfig(
             use_full_attention_mask=use_full_attention_mask,
             include_hiddens=self.include_hiddens,
             skip_logits=self.skip_logits,
+            include_input_ids=self.include_input_ids,
         )
         # TODO (@skothenhill) this is a hack to load the old checkpoint.
         # This should be removed once we have a proper checkpoint conversion
