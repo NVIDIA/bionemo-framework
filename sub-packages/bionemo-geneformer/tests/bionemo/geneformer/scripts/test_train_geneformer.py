6f404090eb7255cf22180441954179bce8c685f8
sichu <sichu@nvidia.com>
2024-12-04 20:36:49 +0000

    reduce geneformer e2e test burden

--- sub-packages/bionemo-geneformer/tests/bionemo/geneformer/scripts/test_train_geneformer.py
+++ sub-packages/bionemo-geneformer/tests/bionemo/geneformer/scripts/test_train_geneformer.py
@@ -46,12 +46,16 @@ def test_val_dataloader_in_main_runs_with_limit_val_batches(tmpdir, limit_val_ba
             data_dir=data_path,
             num_nodes=1,
             devices=1,
-            seq_length=128,
+            seq_length=8,
             result_dir=result_dir,
             wandb_project=None,
             wandb_offline=True,
             num_steps=5,
+<<<<<<< HEAD
             limit_val_batches=1,
+=======
+            limit_val_batches=limit_val_batches,
+>>>>>>> c7291696a (reduce geneformer e2e test burden)
             val_check_interval=2,
             num_dataset_workers=0,
             biobert_spec_option=BiobertSpecOption.bert_layer_local_spec,
