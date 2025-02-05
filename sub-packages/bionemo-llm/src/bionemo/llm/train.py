d0b898c3fccbccec0d34d656303e73dd7fa8035d
sichu <sichu@nvidia.com>
2025-02-05 07:52:41 +0000

    fix pydantic api
    Signed-off-by: sichu <sichu@nvidia.com>

--- sub-packages/bionemo-llm/src/bionemo/llm/train.py
+++ sub-packages/bionemo-llm/src/bionemo/llm/train.py
@@ -252,8 +252,8 @@ def train(
         config=bionemo_model_config,
         tokenizer=data.tokenizer,
         optimizer=optimizer,
-        log_train_ppl=training_config.log_train_ppl,
-        log_val_ppl=training_config.log_val_ppl,
+        train_metric=None,
+        valid_metric=None,
     )
     trainer: nl.Trainer = setup_trainer(parallel_config, training_config, nsys_config=nsys_config)
     nemo_logger: nl.NeMoLogger = nemo_logger_factory(experiment_config, wandb_config=wandb_config)
