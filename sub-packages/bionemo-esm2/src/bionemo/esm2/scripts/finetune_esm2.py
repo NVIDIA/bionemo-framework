034f9dc5ef8dd91d2a7e51b2136d207a8dc35c5b
Farhad Ramezanghorbani <farhadr@nvidia.com>
2025-02-14 22:01:48 +0000

    change limit val batch default
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>

--- sub-packages/bionemo-esm2/src/bionemo/esm2/scripts/finetune_esm2.py
+++ sub-packages/bionemo-esm2/src/bionemo/esm2/scripts/finetune_esm2.py
@@ -634,7 +634,7 @@ def get_parser():
         "--limit-val-batches",
         type=float_or_int_or_none,
         required=False,
-        default=2,
+        default=1.0,
         help="Number of global batches used for validation if int. Fraction of validation dataset if float. Default is 2.",
     )
     parser.add_argument(
