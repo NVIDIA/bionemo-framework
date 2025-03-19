facf1c4755104127d2682a6bd003f96a91320653
Peter St. John <pstjohn@nvidia.com>
2025-03-19 15:04:05 -0700

    add data_dir to train
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- sub-packages/bionemo-amplify/src/bionemo/amplify/train_amplify.py
+++ sub-packages/bionemo-amplify/src/bionemo/amplify/train_amplify.py
@@ -236,7 +236,7 @@ def main(
 
     # Initialize the data module.
     data = AMPLIFYDataModule(
-        train_hf_dataset=hf_load_dataset("chandar-lab/UR100P", split="train"),  # type: ignore
+        train_hf_dataset=hf_load_dataset("chandar-lab/UR100P", data_dir="UniProt", split="train"),  # type: ignore
         valid_hf_dataset=hf_load_dataset("chandar-lab/UR100P", data_dir="UniProt", split="test"),  # type: ignore
         global_batch_size=global_batch_size,
         micro_batch_size=micro_batch_size,
