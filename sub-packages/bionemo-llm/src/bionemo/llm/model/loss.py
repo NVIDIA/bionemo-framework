91f838bff48f652b5afceee554a5c1684c51fb41
Cory Ye <cye@nvidia.com>
2025-05-02 14:56:59 -0700

    Fix masked token loss refactor from NeMo bump.
    Signed-off-by: Cory Ye <cye@nvidia.com>

--- sub-packages/bionemo-llm/src/bionemo/llm/model/loss.py
+++ sub-packages/bionemo-llm/src/bionemo/llm/model/loss.py
@@ -175,43 +175,51 @@ class BERTMLMLossWithReduction(_Nemo2CompatibleLossReduceMixin, MegatronLossRedu
 
         # TODO(@jstjohn) also handle different output keys, like the sequence loss.
 
-        # compute loss
+        # Compute loss over "valid" tokens in the microbatch, i.e. the non-masked tokens.
+        # The loss is not normalized, only potentially reduced via torch.distributed.ReduceOp.SUM
+        # across the context parallel process group, so you need to divide by the number
+        # of non-masked tokens (loss_mask.sum()) to compute the mean reduced loss per token.
         cp_size = parallel_state.get_context_parallel_world_size()
-        loss_for_microbatch = masked_token_loss(unreduced_token_loss, batch["loss_mask"], cp_size)
+        loss_for_microbatch = masked_token_loss(unreduced_token_loss, batch["loss_mask"], cp_size=cp_size)
 
         # If we do not drop the last partial batch of validation, we need to do fancy reduction handling to support
         #  reducing the loss across the data parallel group.
         if self.validation_step and not self.val_drop_last:
-            num_valid_tokens_in_microbatch = batch["loss_mask"].sum()
             if loss_for_microbatch.isnan():
                 # TODO(@jomitchell): Add a unit test for this. This is the case where there are no valid tokens in the microbatch for the loss
                 #  to be computed over, so we expect a NaN loss (divide by zero for a mean) but we make this an expected and non-breaking case,
                 #  re-defining it as a 0 loss. This is standard in NeMo/NeMo2.
                 if batch["loss_mask"].count_nonzero() != 0:
                     raise ValueError("Got NaN loss with non-empty input")
-                loss_sum_for_microbatch = torch.zeros_like(num_valid_tokens_in_microbatch)
+                loss_sum_for_microbatch = torch.zeros_like(batch["num_valid_tokens_in_ub"])
             else:
-                loss_sum_for_microbatch = (
-                    num_valid_tokens_in_microbatch * loss_for_microbatch
-                )  # sum over all valid tokens
+                # The reduced loss is already the sum of all losses from masked_token_loss().
+                loss_sum_for_microbatch = loss_for_microbatch
 
             # In this case we need to store the loss sum as well as the number of valid tokens in the microbatch.
             loss_sum_and_microbatch_size_all_gpu = torch.cat(
                 [
                     loss_sum_for_microbatch.clone().detach().view(1),
-                    Tensor([num_valid_tokens_in_microbatch]).cuda().clone().detach(),
+                    Tensor([batch["num_valid_tokens_in_ub"]]).cuda().clone().detach(),
                 ]
             )
+
+            # Reduce the loss sum across the data parallel group to get the total loss
+            # for all data parallel / distributed microbatches.
             torch.distributed.all_reduce(
                 loss_sum_and_microbatch_size_all_gpu,
                 group=parallel_state.get_data_parallel_group(),
                 op=torch.distributed.ReduceOp.SUM,
             )
+
+            # Return the loss tensor multiplied by the context parallel size,
+            # and the data & context parallel reduced loss sum.
             return loss_for_microbatch * cp_size, {
                 "loss_sum_and_microbatch_size": loss_sum_and_microbatch_size_all_gpu
             }
 
-        # average the losses across the data parallel group, but also return the unreduced loss
+        # Return the loss tensor multiplied by the context parallel size, as well as
+        # the data-parallel averaged loss, i.e. the loss divided by the DP size.
         reduced_loss = average_losses_across_data_parallel_group([loss_for_microbatch])
         return loss_for_microbatch * cp_size, {"avg": reduced_loss}
 
