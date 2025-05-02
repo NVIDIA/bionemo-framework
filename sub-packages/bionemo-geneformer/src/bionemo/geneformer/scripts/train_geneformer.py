5270fec896d51d9a89bd2fdfe6a6b0005c7d6958
Polina Binder <pbinder@nvidia.com>
2025-05-02 13:59:20 -0700

    geneformer callbacks
    Signed-off-by: Polina Binder <pbinder@nvidia.com>

--- sub-packages/bionemo-geneformer/src/bionemo/geneformer/scripts/train_geneformer.py
+++ sub-packages/bionemo-geneformer/src/bionemo/geneformer/scripts/train_geneformer.py
@@ -33,6 +33,7 @@ from nemo import lightning as nl
 from nemo.collections import llm
 from nemo.lightning import resume
 from nemo.lightning.pytorch import callbacks as nl_callbacks
+from nemo.lightning.pytorch.callbacks.flops_callback import FLOPsMeasurementCallback
 from nemo.lightning.pytorch.optim import MegatronOptimizerModule
 from nemo.lightning.pytorch.optim.lr_scheduler import CosineAnnealingScheduler
 from nemo.utils import logging
@@ -94,6 +95,7 @@ def main(
     nsys_start_step: int = 0,
     nsys_end_step: Optional[int] = None,
     nsys_ranks: List[int] = [0],
+    create_tflops_callback: bool = False,
     config_class: Type[BioBertConfig] = GeneformerConfig,
     log_every_n_steps: int = 50,
     gc_interval: int = 0,
@@ -122,6 +124,7 @@ def main(
         experiment_name (str): experiment name, this is the name used for the wandb run, and the sub-directory of the
             result_dir that stores the logs and checkpoints.
         accumulate_grad_batches (int): if requested, gradients are only updated every `accumulate_grad_batches` steps.
+        create_tflops_callback (bool): if True, a callback that logs the tera-FLOPS per second per GPU during training is added.
         config_class (Type[BioBertConfig]): which model config do you want to train?
         metric_to_monitor_for_checkpoints (str): which metric do you want to monitor for checkpoints?
         nemo1_init_path (str): if you have a nemo1 checkpoint you want to initialize the model weights from, you can
@@ -345,6 +348,19 @@ def main(
         )
     else:
         checkpoint_callback = None
+
+    if create_tflops_callback:
+        # Add callback that logs the tera-FLOPS per second per GPU during training.
+        class tflops_data_module:
+            def __init__(self, tokenizer_vocab_size, global_batch_size):
+                self.tokenizer_vocab_size = tokenizer_vocab_size
+                self.global_batch_size = global_batch_size
+
+        callbacks.append(
+            FLOPsMeasurementCallback(
+                geneformer_config, tflops_data_module(tokenizer.vocab_size, global_batch_size), "bert"
+            )
+        )
 
     # Setup the logger and train the model
     nemo_logger = setup_nemo_lightning_logger(
@@ -665,6 +681,12 @@ def get_parser():
         help="Activate this and make sure a small training loop runs, this tells you that your settings are not "
         "triggering regular recompilations which can be very expensive for fused gpu kernels.",
     )
+    parser.add_argument(
+        "--create-tflops-callback",
+        action="store_true",
+        default=False,
+        help="Enable tflops calculation callback for Geneformer. Defaults to False.",
+    )
 
     return parser
 
@@ -708,6 +730,7 @@ def entrypoint():
         nsys_end_step=args.nsys_end_step,
         nsys_ranks=args.nsys_ranks,
         create_checkpoint_callback=args.create_checkpoint_callback,
+        create_tflops_callback=args.create_tflops_callback,
         restore_from_checkpoint_path=args.restore_from_checkpoint_path,
         config_class=args.training_model_config_class,
         save_last_checkpoint=args.save_last_checkpoint,
