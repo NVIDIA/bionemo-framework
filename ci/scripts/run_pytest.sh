ab04784f28ba7a08f8526f523fc037334c2e3e38
Bruno Alvisio <balvisio@nvidia.com>
2025-06-15 22:43:09 +0000

    run jupyter notebooks in different pytests

--- ci/scripts/run_pytest.sh
+++ ci/scripts/run_pytest.sh
@@ -93,9 +93,6 @@ PYTEST_OPTIONS=(
 
 # Define test directories
 TEST_DIRS=(./sub-packages/bionemo-evo2/)
-if [[ "$NO_NBVAL" != true && "$SKIP_DOCS" != true ]]; then
-    TEST_DIRS+=(docs/)
-fi
 
 echo "Test directories: ${TEST_DIRS[*]}"
 
@@ -116,7 +113,8 @@ for dir in "${TEST_DIRS[@]}"; do
     echo "Running pytest in $dir"
     # Run pytest but don't exit on failure - we'll handle the exit code separately. This is needed because our script is
     #  running in pipefail mode and pytest will exit with a non-zero exit code if it finds no tests.
-    { pytest "${PYTEST_OPTIONS[@]}" -s -x -v --junitxml=$(basename $dir).junit.xml -o junit_family=legacy "$dir"; exit_code=$?; } || true
+    { pytest "${PYTEST_OPTIONS[@]}" -s -x -v --junitxml=$(basename $dir).junit.xml -o junit_family=legacy "./sub-packages/bionemo-evo2/examples/fine-tuning-tutorial.ipynb"; exit_code=$?; } || true
+    { pytest "${PYTEST_OPTIONS[@]}" -s -x -v --junitxml=$(basename $dir).junit.xml -o junit_family=legacy "./sub-packages/bionemo-evo2/examples/zeroshot_brca1.ipynb"; exit_code=$?; } || true
 
     if [[ $exit_code -ne 0 ]]; then
         if [[ "$ALLOW_NO_TESTS" == true && $exit_code -eq 5 ]]; then
