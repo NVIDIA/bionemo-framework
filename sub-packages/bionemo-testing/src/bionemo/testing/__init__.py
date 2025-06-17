e16092b13f93f348093105a8b66b25c433abd948
My Le <mvle@nvidia.com>
2025-06-17 12:05:33 +0700

    Remove unused import and update __all__ in bionemo testing module

--- sub-packages/bionemo-testing/src/bionemo/testing/__init__.py
+++ sub-packages/bionemo-testing/src/bionemo/testing/__init__.py
@@ -12,7 +12,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from bionemo.testing.tensorboard import verify_tensorboard_logs
-
-__all__ = ["verify_tensorboard_logs"]
