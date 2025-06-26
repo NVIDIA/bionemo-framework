79e267c83ba2a4654e4984e0ec9a3635fd181014
Steven <skothenhill@nvidia.com>
2025-06-26 16:07:02 -0700

    revert to simpler fix that does not change masking procedure
    Signed-off-by: Steven <skothenhill@nvidia.com>

--- sub-packages/bionemo-llm/src/bionemo/llm/model/loss.py
+++ sub-packages/bionemo-llm/src/bionemo/llm/model/loss.py
@@ -97,6 +97,10 @@ class BERTMLMLossWithReduction(MegatronLossReduction):  # noqa: D101
 
         num_valid_tokens = num_valid_tokens.clone().detach().to(torch.int)
         loss_sum_and_ub_size = torch.cat([loss_sum.clone().detach().view(1), num_valid_tokens.view(1)])
+
+        if num_valid_tokens.item() == 0:
+            num_valid_tokens = torch.tensor(1, device=num_valid_tokens.device, dtype=num_valid_tokens.dtype)
+
         return loss_sum, num_valid_tokens, {"loss_sum_and_ub_size": loss_sum_and_ub_size}
 
     def reduce(self, losses_reduced_per_micro_batch) -> torch.Tensor:
