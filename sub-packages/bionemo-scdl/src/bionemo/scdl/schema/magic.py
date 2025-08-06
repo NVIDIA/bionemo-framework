58774570f28fe7b873279dbe1b49c4e9631f7a2c
Eric T. Dawson <edawson@nvidia.com>
2025-08-06 12:31:40 -0400

    Add a magic number
    Signed-off-by: Eric T. Dawson <edawson@nvidia.com>

--- sub-packages/bionemo-scdl/src/bionemo/scdl/schema/magic.py
+++ sub-packages/bionemo-scdl/src/bionemo/scdl/schema/magic.py
@@ -1,3 +1,11 @@
 
+"""
+SCDL Magic Number Definition
 
-MAGIC_NUMBER: bytes = b'SCDL'
+This module defines the magic number for SCDL archives as specified in the schema.
+The magic number 'SCDL' (0x5343444C) identifies valid SCDL archive headers.
+"""
+
+# Magic number as specified in SCDL schema: 'SCDL' (0x5343444C)
+SCDL_MAGIC_NUMBER: bytes = b'SCDL'
+
