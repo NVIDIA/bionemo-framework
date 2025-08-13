22f7b3b8ee52069cd07a9e7eec0a5389c3ee5b4e
Peter St. John <pstjohn@nvidia.com>
2025-08-13 09:30:29 -0700

    remove extra ruff check
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- ci/scripts/static_checks.sh
+++ ci/scripts/static_checks.sh
@@ -5,12 +5,6 @@ set -xueo pipefail
 REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
 cd $REPOSITORY_ROOT
 
-echo "Running ruff checks"
-set +e
-ruff check scripts/ sub-packages/ docs/
-E_RUFF_CHECK="$?"
-set -e
-
 echo "Running tach checks"
 set +e
 tach check
@@ -28,10 +22,6 @@ ANY_FAILURE=0
 if [[ "${E_PRE_COMMIT}" != "0" ]]; then
     ANY_FAILURE=1
     echo "ERROR: pre-commit hooks failed! (exit: ${E_PRE_COMMIT})"
-fi
-if [[ "${E_RUFF_CHECK}" != "0" ]]; then
-  ANY_FAILURE=1
-  echo "ERROR: ruff check failed! (exit: ${E_RUFF_CHECK})"
 fi
 if [[ "${E_TACH_CHECK}" != "0" ]]; then
   ANY_FAILURE=1
