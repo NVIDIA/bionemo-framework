76e0d55aaaed2637c7513dd6b161f6b6dd7cd101
Eric T. Dawson <edawson@nvidia.com>
2025-08-15 09:12:53 -0400

    Add license + init.py
    Signed-off-by: Eric T. Dawson <edawson@nvidia.com>

--- sub-packages/bionemo-scdl/src/bionemo/scdl/schema/__init__.py
+++ sub-packages/bionemo-scdl/src/bionemo/scdl/schema/__init__.py
@@ -12,14 +12,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-SCDL Magic Number Definition
-
-This module defines the magic number for SCDL archives as specified in the schema.
-The magic number 'SCDL' (0x5343444C) identifies valid SCDL archive headers.
-"""
-
-# Magic number as specified in SCDL schema: 'SCDL' (0x5343444C)
-SCDL_MAGIC_NUMBER: bytes = b'SCDL'
-
