8292970535064b46151a3f06fe2a603bf8ffa677
Anton Vorontsov <avorontsov@nvidia.com>
2025-08-19 16:09:37 -0700

    speed_test: Use bionemo's inference.py

--- speed_test.py
+++ speed_test.py
@@ -26,74 +26,9 @@ from megatron.core.inference.common_inference_params import CommonInferenceParam
 from nemo.collections.llm import generate
 from nemo.utils import logging
 
-def get_inference_params():
-    # Below are parameters that need to be tuned based on GPU memory size.
-    return {
-        # Affects KV cache size, can cause OOM easily, set to 1 to save memory (i.e. running 40b model)
-        "inference_max_requests": 1,
-    }
-
-def get_trainer(pipeline_parallel=1):
-    import nemo.lightning as nl
-
-    fp8 = True
-    full_fp8 = False
-    return nl.Trainer(
-        accelerator="gpu",
-        devices=pipeline_parallel,
-        strategy=nl.MegatronStrategy(
-            tensor_model_parallel_size=1,
-            pipeline_model_parallel_size=pipeline_parallel,
-            context_parallel_size=1,
-            pipeline_dtype=torch.bfloat16,
-            ckpt_load_optimizer=False,
-            ckpt_save_optimizer=False,
-            ckpt_async_save=False,
-            save_ckpt_format="torch_dist",
-            ckpt_load_strictness="log_all",
-        ),
-        log_every_n_steps=1,
-        limit_val_batches=10,
-        num_sanity_val_steps=0,
-        plugins=nl.MegatronMixedPrecision(
-            precision="bf16-mixed",
-            params_dtype=torch.bfloat16,
-            # Only use FP8 in this plugin when using full FP8 precision and FP8.
-            #   Otherwise use vortex_style_fp8 in the model config.
-            fp8="hybrid" if fp8 and full_fp8 else None,
-            fp8_amax_history_len=16 if fp8 and full_fp8 else 1,
-            fp8_amax_compute_algo="max" if fp8 and full_fp8 else "most_recent",
-        ),
-    )
-
-def get_model_and_tokenizer(ckpt_dir="ckpt/7b", vortex_style_fp8=True):
-    trainer = get_trainer()
-    from nemo.collections.llm import inference
-
-    inference_wrapped_model, mcore_tokenizer = inference.setup_model_and_tokenizer(
-        path=ckpt_dir,
-        trainer=trainer,
-        params_dtype=torch.bfloat16,
-        inference_batch_times_seqlen_threshold=8192,  # TODO
-        inference_max_seq_length=8192,  # TODO
-        vortex_style_fp8=vortex_style_fp8,
-        fp32_residual_connection=False,
-        # use_te_rng_tracker=True,
-        # te_rng_tracker=True,
-        # inference_rng_tracker=True,
-        # enable_cuda_graph=True,
-        # cudagraph_rng_tracker=True,
-        enable_flash_decode=True,
-        cuda_graph=True,
-        recompute_granularity=None,
-        recompute_num_layers=None,
-        recompute_method=None,
-        **get_inference_params(),
-    )
-    return inference_wrapped_model, mcore_tokenizer
-
 def do_generate(n=300):
-    inference_wrapped_model, mcore_tokenizer = get_model_and_tokenizer()
+    from bionemo.evo2.inference import get_model_and_tokenizer
+    inference_wrapped_model, mcore_tokenizer = get_model_and_tokenizer(ckpt_name="evo2/7b-1m:1.0")
 
     from megatron.core.inference.common_inference_params import CommonInferenceParams
     from nemo.collections.llm.inference import generate
