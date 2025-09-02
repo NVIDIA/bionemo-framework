09207b586f669052a5dfa4a850656f749b81b64b
Brian Roland <broland@nvidia.com>
2025-09-02 17:21:29 +0000

    br: train script updated with custom metrics [skip ci]
    Signed-off-by: Brian Roland <broland@nvidia.com>

--- sub-packages/bionemo-evo2/src/bionemo/evo2/run/train.py
+++ sub-packages/bionemo-evo2/src/bionemo/evo2/run/train.py
@@ -54,6 +54,26 @@ from bionemo.evo2.utils.logging.callbacks import TEVCallback
 from bionemo.llm.utils.datamodule_utils import infer_global_batch_size
 from bionemo.llm.utils.logger_utils import WandbConfig, setup_nemo_lightning_logger
 
+from bionemo.evo2.utils.logging.bnm_module_hook_manager import BnmModuleHookManager
+
+
+class HyenaModelWithCustomMetrics(llm.HyenaModel):
+    
+    def configure_model(self, vp_stage: Optional[int] = None) -> None:
+        """Add additional configuration for HyenaModel(GPTModel), after GPTModel.configure_model().
+        
+        When this method is called, self.module is the HyenaModel(LanguageModule(MegatronModel))
+        
+        """
+        super(llm.HyenaModel, self).configure_model(vp_stage=vp_stage)
+
+        self.bnm_module_hook_manager = BnmModuleHookManager()
+
+        self.bnm_module_hook_manager.configure_hooks(
+            root_module=self.module,
+            forward_pre_hook_types=["input_shapes"],
+            forward_hook_types=["output_shapes"],
+        )
 
 torch._dynamo.config.suppress_errors = True
 
@@ -631,7 +651,8 @@ def train(args: argparse.Namespace) -> nl.Trainer:
         if args.lora_finetune:
             lora_transform = Evo2LoRA(peft_ckpt_path=args.lora_checkpoint_path)
 
-        model = llm.HyenaModel(model_config, tokenizer=data_module.tokenizer, model_transform=lora_transform)
+        #model = llm.HyenaModel(model_config, tokenizer=data_module.tokenizer, model_transform=lora_transform)
+        model = HyenaModelWithCustomMetrics(model_config, tokenizer=data_module.tokenizer, model_transform=lora_transform)
     else:  # mamba
         if args.no_weight_decay_embeddings:
             config_modifiers_init["hyena_no_weight_decay_cond_fn"] = mamba_no_weight_decay_cond_with_embeddings
