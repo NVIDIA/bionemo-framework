d29d3ce90cc02c7d7d3e72f3cb43162b73ae6a1a
Jonathan Mitchell <jomitchell@nvidia.com>
2025-10-27 15:22:43 -0700

    x
    Signed-off-by: Jonathan Mitchell <jomitchell@nvidia.com>

--- bionemo-recipes/recipes/codonfm_ptl_te/experiment_scripts/pretraining/encodon_filtered/mlm/encodon_80m.sh
+++ bionemo-recipes/recipes/codonfm_ptl_te/experiment_scripts/pretraining/encodon_filtered/mlm/encodon_80m.sh
@@ -2,10 +2,10 @@
 set -ex
 
 learning_rate=1e-4
-num_nodes=1
-num_gpus=2
-train_batch_size=4
-val_batch_size=4
+num_nodes=4
+num_gpus=8
+train_batch_size=32
+val_batch_size=32
 effective_batch_size=$((train_batch_size * num_gpus * num_nodes))
 num_workers=12
 
@@ -16,7 +16,7 @@ exp_name="encodon_80m_latest_${learning_rate}_${effective_batch_size}_nopathogen
 python -m src.runner pretrain \
     --exp_name "$exp_name" \
     --model_name encodon_80m \
-    --data_path /data/codonfm/downloadsnight/postprocessed_optimized_full/ \
+    --data_path /data/ncbi/processed_unfiltered/ \
     --process_item mlm_memmap \
     --dataset_name CodonMemmapDataset \
     --lr $learning_rate \
