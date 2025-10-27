d57b87fc98b4caad5478fec7203f88123c4818cc
Jonathan Mitchell <jomitchell@nvidia.com>
2025-10-27 15:18:28 -0700

    Adds note to readme about SM120 and THD - Also fixes up some of the bash scripts so that its clear what must be done to enable wandb
    Signed-off-by: Jonathan Mitchell <jomitchell@nvidia.com>

--- bionemo-recipes/recipes/codonfm_ptl_te/experiment_scripts/pretraining/encodon_filtered/mlm/encodon_80m.sh
+++ bionemo-recipes/recipes/codonfm_ptl_te/experiment_scripts/pretraining/encodon_filtered/mlm/encodon_80m.sh
@@ -2,19 +2,21 @@
 set -ex
 
 learning_rate=1e-4
-num_nodes=4
-num_gpus=8
-train_batch_size=32
-val_batch_size=32
+num_nodes=1
+num_gpus=2
+train_batch_size=4
+val_batch_size=4
 effective_batch_size=$((train_batch_size * num_gpus * num_nodes))
 num_workers=12
 
 exp_name="encodon_80m_latest_${learning_rate}_${effective_batch_size}_nopathogen"
 
+# Note if you would like to use WandB please add --enable_wandb, --project_name and --entity.
+
 python -m src.runner pretrain \
     --exp_name "$exp_name" \
     --model_name encodon_80m \
-    --data_path /data/ncbi/processed_unfiltered/ \
+    --data_path /data/codonfm/downloadsnight/postprocessed_optimized_full/ \
     --process_item mlm_memmap \
     --dataset_name CodonMemmapDataset \
     --lr $learning_rate \
@@ -30,4 +32,3 @@ python -m src.runner pretrain \
     --split_name_prefix nopathogen \
     --use_transformer_engine \
     --checkpoints_dir results/checkpoints/${exp_name} \
-    --enable_wandb
