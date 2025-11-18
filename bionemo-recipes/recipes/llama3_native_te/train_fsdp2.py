4f3f09fea302a5a0fa88dd74f3dafa377d35da6e
Savitha Srinivasan <savithas@nvidia.com>
2025-11-18 01:54:30 +0000

    fix bug with load checkpoint when use_stateful_Dataloader=false

--- bionemo-recipes/recipes/llama3_native_te/train_fsdp2.py
+++ bionemo-recipes/recipes/llama3_native_te/train_fsdp2.py
@@ -111,7 +111,7 @@ def main(args: DictConfig) -> float | None:  # noqa: C901
             scheduler=scheduler,
             ckpt_path=ckpt_path,
             dist_config=dist_config,
-            dataloader=train_dataloader if args.dataset.use_stateful_dataloader else None,
+            dataloader=train_dataloader,
         )
     else:
         start_step = 0
