0702f70c83b800f7be0d2554e532462c383c9ee9
Jonathan Mitchell <jomitchell@ipp1-1334.ipp1a1.colossus.nvidia.com>
2025-12-01 12:50:02 -0800

    tries flat device mesh
    Signed-off-by: Jonathan Mitchell <jomitchell@ipp1-1334.ipp1a1.colossus.nvidia.com>

--- bionemo-recipes/recipes/esm2_native_te/train_fsdp2_cp.py
+++ bionemo-recipes/recipes/esm2_native_te/train_fsdp2_cp.py
@@ -76,10 +76,13 @@ def main(args: DictConfig) -> float | None:  # noqa: C901
     # Total ranks = cp_size * ddp_size = world_size
     device_mesh = init_device_mesh(
         "cuda",
-        mesh_shape=(args.cp_size, ddp_size),
-        mesh_dim_names=("cp", "dp"),
+        mesh_shape=(ddp_size, args.cp_size),
+        mesh_dim_names=("dp", "cp"),
     )
 
+    cp_dp_flat_mesh = device_mesh["dp", "cp"]._flatten(mesh_dim_name="dp_shard_cp")
+
+    
     cp_group = device_mesh["cp"].get_group()
     cp_rank = device_mesh.get_local_rank("cp")
 
@@ -110,14 +113,14 @@ def main(args: DictConfig) -> float | None:  # noqa: C901
     # Fully shard takes in a DeviceMesh object, which is a 2D mesh of dimensions (CP_dimension, DDP_dimension).
     # FSDP2 will shard the model across the DDP (dim=1) dimension and then duplicate across the CP (dim=0) dimension.
     for layer in transformer_stack:
-        fully_shard(layer, mesh=device_mesh)
+        fully_shard(layer, mesh=cp_dp_flat_mesh)
         # Set CP group for layer if CP is enabled.
         if args.cp_size > 1:
             logger.debug(f"Rank {dist_config.rank}: Setting CP group for layer {layer}")
             layer.set_context_parallel_group(
                 cp_group, torch.distributed.get_process_group_ranks(device_mesh["cp"].get_group()), torch.cuda.Stream()
             )
-    fully_shard(model, mesh=device_mesh)
+    fully_shard(model, mesh=cp_dp_flat_mesh)
 
     # Create optimizer. Convert OmegaConf to regular dict to avoid serialization issues (BIONEMO-2873).
     optimizer = AdamW(model.parameters(), **OmegaConf.to_container(args.adamw_kwargs, resolve=True))  # type: ignore
