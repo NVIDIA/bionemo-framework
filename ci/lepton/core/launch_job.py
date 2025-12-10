07ab73fc5a190c43f32003706d541e7bbceab24d
Jared Wilber <jwilber@nvidia.com>
2025-12-10 14:46:04 -0800

    lint
    Signed-off-by: Jared Wilber <jwilber@nvidia.com>

--- ci/lepton/core/launch_job.py
+++ ci/lepton/core/launch_job.py
@@ -84,11 +84,11 @@ def launch_single_job(client, cfg: DictConfig):
     template_type = getattr(cfg, "template_type", "convergence_tests")
 
     # trigger with logging to kratos, if enabled. Default to True.
-    if getattr(cfg, 'log_to_kratos', True):
-        print(f"Logging to kratos")
+    if getattr(cfg, "log_to_kratos", True):
+        print("Logging to kratos")
         rendered = render_launcher_string(cfg.script, full_cfg_json, template=template_type)
-    else: # don't log to kratos
-        print(f"Not logging to kratos")
+    else:  # don't log to kratos
+        print("Not logging to kratos")
         rendered = cfg.script
 
     command = ["bash", "-c", rendered]
