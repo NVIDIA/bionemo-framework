ab6de332354b4287635b96fbb0544d7d0485a6c4
Peter St. John <pstjohn@nvidia.com>
2026-01-02 09:16:08 -0800

    trying new meta device init
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- bionemo-recipes/models/esm2/tests/test_meta_device_init.py
+++ bionemo-recipes/models/esm2/tests/test_meta_device_init.py
@@ -24,14 +24,10 @@ By explicitly calling `_init_weights` after `to_empty`, we ensure that parameter
 consistent training behavior regardless of whether meta device initialization is used.
 """
 
-import os
-import subprocess
-
 import pytest
 import torch
 import transformer_engine.pytorch
-from torch.distributed.fsdp import fully_shard
-from torch.distributed.tensor import DTensor
+from transformer_engine.pytorch.tensor import QuantizedTensor
 from transformers import AutoConfig, set_seed
 
 from esm.modeling_esm_te import NVEsmConfig, NVEsmForMaskedLM
@@ -43,7 +39,9 @@ requires_multi_gpu = pytest.mark.skipif(
 )
 
 
-def verify_model_parameters_initialized_correctly(model: NVEsmForMaskedLM, atol=1e-3, rtol=1e-4):
+def verify_model_parameters_initialized_correctly(
+    model: NVEsmForMaskedLM, atol=1e-3, rtol=1e-4, should_be_fp8: bool = False
+):
     config = model.config
 
     for name, parameter in model.named_parameters():
@@ -66,6 +64,8 @@ def verify_model_parameters_initialized_correctly(model: NVEsmForMaskedLM, atol=
                 module.weight.std().item(), config.initializer_range, atol=atol, rtol=rtol, msg=msg
             )
             torch.testing.assert_close(module.bias, torch.zeros_like(module.bias), msg=msg)
+            if should_be_fp8:
+                assert isinstance(module.weight, QuantizedTensor), f"Module {name} weight is not a QuantizedTensor"
 
         elif isinstance(module, transformer_engine.pytorch.LayerNormLinear):
             torch.testing.assert_close(module.weight.mean().item(), 0.0, atol=atol, rtol=rtol, msg=msg)
@@ -75,6 +75,8 @@ def verify_model_parameters_initialized_correctly(model: NVEsmForMaskedLM, atol=
             torch.testing.assert_close(module.bias, torch.zeros_like(module.bias), msg=msg)
             torch.testing.assert_close(module.layer_norm_weight, torch.ones_like(module.layer_norm_weight), msg=msg)
             torch.testing.assert_close(module.layer_norm_bias, torch.zeros_like(module.layer_norm_bias), msg=msg)
+            if should_be_fp8:
+                assert isinstance(module.weight, QuantizedTensor), f"Module {name} weight is not a QuantizedTensor"
 
         elif isinstance(module, transformer_engine.pytorch.LayerNormMLP):
             torch.testing.assert_close(module.fc1_weight.mean().item(), 0.0, atol=atol, rtol=rtol, msg=msg)
@@ -89,6 +91,13 @@ def verify_model_parameters_initialized_correctly(model: NVEsmForMaskedLM, atol=
             torch.testing.assert_close(module.fc2_bias, torch.zeros_like(module.fc2_bias), msg=msg)
             torch.testing.assert_close(module.layer_norm_weight, torch.ones_like(module.layer_norm_weight), msg=msg)
             torch.testing.assert_close(module.layer_norm_bias, torch.zeros_like(module.layer_norm_bias), msg=msg)
+            if should_be_fp8:
+                assert isinstance(module.fc1_weight, QuantizedTensor), (
+                    f"Module {name} fc1_weight is not a QuantizedTensor"
+                )
+                assert isinstance(module.fc2_weight, QuantizedTensor), (
+                    f"Module {name} fc2_weight is not a QuantizedTensor"
+                )
 
         elif isinstance(module, torch.nn.LayerNorm):
             torch.testing.assert_close(module.weight, torch.ones_like(module.weight), msg=msg)
@@ -122,11 +131,7 @@ def test_meta_init():
         assert parameter.device == torch.device("meta"), f"Parameter {name} is not on the meta device"
 
     # Move the model to the cuda device and initialize the parameters
-    model.to_empty(device="cuda")
-    for module in model.modules():
-        if hasattr(module, "reset_parameters"):
-            module.reset_parameters()
-    model.apply(model._init_weights)
+    model.init_from_meta_device()
 
     verify_model_parameters_initialized_correctly(model)
 
@@ -140,7 +145,7 @@ def test_cuda_fp8_init(fp8_recipe):
 
     model.to("cuda")
 
-    verify_model_parameters_initialized_correctly(model, atol=1e-2)
+    verify_model_parameters_initialized_correctly(model, atol=1e-2, should_be_fp8=True)
 
 
 def test_meta_fp8_init(fp8_recipe):
@@ -151,103 +156,99 @@ def test_meta_fp8_init(fp8_recipe):
         model = NVEsmForMaskedLM(config)
 
     # Move the model to the cuda device and initialize the parameters
-    model.to_empty(device="cuda")
-    for module in model.modules():
-        if hasattr(module, "reset_parameters"):
-            module.reset_parameters()
-    model.apply(model._init_weights)
+    model.init_from_meta_device()
 
-    verify_model_parameters_initialized_correctly(model)
+    verify_model_parameters_initialized_correctly(model, should_be_fp8=True)
 
 
-@pytest.mark.parametrize("num_gpus", [1, pytest.param(2, marks=requires_multi_gpu)])
-def test_meta_device_init_after_fully_shard(num_gpus: int):
-    cmd = [
-        "torchrun",
-        f"--nproc_per_node={num_gpus}",
-        os.path.relpath(__file__),
-    ]
+# @pytest.mark.parametrize("num_gpus", [1, pytest.param(2, marks=requires_multi_gpu)])
+# def test_meta_device_init_after_fully_shard(num_gpus: int):
+#     cmd = [
+#         "torchrun",
+#         f"--nproc_per_node={num_gpus}",
+#         os.path.relpath(__file__),
+#     ]
 
-    result = subprocess.run(
-        cmd,
-        check=False,
-        text=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        timeout=240,
-    )
+#     result = subprocess.run(
+#         cmd,
+#         check=False,
+#         text=True,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#         timeout=240,
+#     )
 
-    if result.returncode != 0:
-        print(f"STDOUT:\n{result.stdout}")
-        print(f"STDERR:\n{result.stderr}")
-        pytest.fail(f"Command failed with exit code {result.returncode}")
+#     if result.returncode != 0:
+#         print(f"STDOUT:\n{result.stdout}")
+#         print(f"STDERR:\n{result.stderr}")
+#         pytest.fail(f"Command failed with exit code {result.returncode}")
 
 
-if __name__ == "__main__":
-    torch.distributed.init_process_group(backend="cuda:nccl")
-    torch.cuda.set_device(torch.distributed.get_rank())
+# if __name__ == "__main__":
+#     torch.distributed.init_process_group(backend="cuda:nccl")
+#     torch.cuda.set_device(torch.distributed.get_rank())
 
-    config = NVEsmConfig(**AutoConfig.from_pretrained("facebook/esm2_t6_8M_UR50D").to_dict())
+#     config = NVEsmConfig(**AutoConfig.from_pretrained("facebook/esm2_t6_8M_UR50D").to_dict())
 
-    set_seed(42)
+#     set_seed(42)
 
-    with torch.device("meta"):
-        model_meta_init = NVEsmForMaskedLM(config)
+#     with torch.device("meta"):
+#         model_meta_init = NVEsmForMaskedLM(config)
 
-    for layer in model_meta_init.esm.encoder.layers:
-        fully_shard(layer)
-    fully_shard(model_meta_init)
+#     for layer in model_meta_init.esm.encoder.layers:
+#         fully_shard(layer)
+#     fully_shard(model_meta_init)
 
-    # Assert parameters are actually on the meta device
-    for name, parameter in model_meta_init.named_parameters():
-        assert parameter.device == torch.device("meta"), f"Parameter {name} is not on the meta device"
+#     # Assert parameters are actually on the meta device
+#     for name, parameter in model_meta_init.named_parameters():
+#         assert parameter.device == torch.device("meta"), f"Parameter {name} is not on the meta device"
 
-    model_meta_init.to_empty(device="cuda")
-    model_meta_init.apply(model_meta_init._init_weights)
+#     model_meta_init.to_empty(device="cuda")
+#     model_meta_init.apply(model_meta_init._init_weights)
 
-    # Assert parameters are actually on the cuda device after to_empty
-    for name, parameter in model_meta_init.named_parameters():
-        assert str(parameter.device).startswith("cuda"), f"Parameter {name} is not on the cuda device"
+#     # Assert parameters are actually on the cuda device after to_empty
+#     for name, parameter in model_meta_init.named_parameters():
+#         assert str(parameter.device).startswith("cuda"), f"Parameter {name} is not on the cuda device"
 
-    set_seed(42)
-    model_normal_init = NVEsmForMaskedLM(config)
+#     set_seed(42)
+#     model_normal_init = NVEsmForMaskedLM(config)
 
-    for layer in model_normal_init.esm.encoder.layers:
-        fully_shard(layer)
-    fully_shard(model_normal_init)
+#     for layer in model_normal_init.esm.encoder.layers:
+#         fully_shard(layer)
+#     fully_shard(model_normal_init)
 
-    state_dict_meta_init = model_meta_init.state_dict()
-    state_dict_normal_init = model_normal_init.state_dict()
+#     state_dict_meta_init = model_meta_init.state_dict()
+#     state_dict_normal_init = model_normal_init.state_dict()
 
-    for key in state_dict_meta_init.keys():
-        if key.endswith("_extra_state"):
-            continue
+#     for key in state_dict_meta_init.keys():
+#         if key.endswith("_extra_state"):
+#             continue
 
-        meta_tensor = state_dict_meta_init[key]
-        normal_tensor = state_dict_normal_init[key]
+#         meta_tensor = state_dict_meta_init[key]
+#         normal_tensor = state_dict_normal_init[key]
 
-        torch.testing.assert_close(
-            normal_tensor.mean(),
-            meta_tensor.mean(),
-            atol=1e-3,
-            rtol=1e-4,
-            msg=lambda x: f"Mean mismatch for parameter {key}: {x}",
-        )
+#         torch.testing.assert_close(
+#             normal_tensor.mean(),
+#             meta_tensor.mean(),
+#             atol=1e-3,
+#             rtol=1e-4,
+#             msg=lambda x: f"Mean mismatch for parameter {key}: {x}",
+#         )
 
-        if isinstance(normal_tensor, DTensor) and isinstance(meta_tensor, DTensor):
-            torch.testing.assert_close(
-                normal_tensor.full_tensor().std(),
-                meta_tensor.full_tensor().std(),
-                atol=1e-2,
-                rtol=1e-4,
-                msg=lambda x: f"Std mismatch for parameter {key}: {x}",
-            )
+#         if isinstance(normal_tensor, DTensor) and isinstance(meta_tensor, DTensor):
+#             torch.testing.assert_close(
+#                 normal_tensor.full_tensor().std(),
+#                 meta_tensor.full_tensor().std(),
+#                 atol=1e-2,
+#                 rtol=1e-4,
+#                 msg=lambda x: f"Std mismatch for parameter {key}: {x}",
+#             )
 
-        else:
-            torch.testing.assert_close(
-                normal_tensor.std(),
-                meta_tensor.std(),
-                atol=1e-2,
-                rtol=1e-4,
-                msg=lambda x: f"Std mismatch for parameter {key}: {x}",
-            )
+#         else:
+#             torch.testing.assert_close(
+#                 normal_tensor.std(),
+#                 meta_tensor.std(),
+#                 atol=1e-2,
+#                 rtol=1e-4,
+#                 msg=lambda x: f"Std mismatch for parameter {key}: {x}",
+#             )
