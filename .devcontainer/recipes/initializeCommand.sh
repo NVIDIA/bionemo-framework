9d56260d8513fad6c22181737e2ec45088eae5f8
Peter St. John <pstjohn@nvidia.com>
2026-01-27 15:20:58 -0800

    add claude code with nvidia inference api
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- .devcontainer/recipes/initializeCommand.sh
+++ .devcontainer/recipes/initializeCommand.sh
@@ -7,10 +7,11 @@ mkdir -p ~/.cache/pre-commit-devcontainer
 mkdir -p ~/.gnupg
 mkdir -p ~/.config
 mkdir -p ~/.cursor
-mkdir -p ~/.claude
-[ ! -f ~/.netrc ] && touch ~/.netrc
 
+[ ! -f ~/.netrc ] && touch ~/.netrc
+[ ! -f ~/.nvidia-api-key ] && touch ~/.nvidia-api-key
+[ ! -f ~/.claude-devcontainer.json ] && touch ~/.claude-devcontainer.json
 [ ! -f ~/.bash_history_devcontainer ] && touch ~/.bash_history_devcontainer
-[ ! -f ~/.claude.json ] && touch ~/.claude.json
+
 
 exit 0
