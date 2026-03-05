179ce898978c847ebc3adfd5cd7e6b682f85f4ef
Peter St. John <pstjohn@nvidia.com>
2026-03-05 08:20:10 -0700

    assert fp32 for rope embeddings, misc test fixes
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- bionemo-recipes/recipes/esm2_accelerate_te/example_8m_checkpoint/esm_nv.py
+++ bionemo-recipes/recipes/esm2_accelerate_te/example_8m_checkpoint/esm_nv.py
@@ -197,6 +197,7 @@ class NVEsmEncoder(nn.Module):
         with torch.autocast(device_type="cuda", enabled=False):
             te_rope_emb = self.rotary_embeddings(max_seq_len=self.config.max_position_embeddings)
             te_rope_emb = te_rope_emb.to(hidden_states.device, non_blocking=True)
+            assert te_rope_emb.dtype == torch.float32, "Rotary embeddings should be in float32 for optimal performance"
 
         for layer_module in self.layers:
             if kwargs.get("output_hidden_states", False):
@@ -374,7 +375,7 @@ class NVEsmModel(NVEsmPreTrainedModel):
         )
         encoder_outputs = self.encoder(
             embedding_output,
-            attention_mask=extended_attention_mask,
+            attention_mask=None if self.config.attn_input_format == "thd" else extended_attention_mask,
             **kwargs,
         )
         sequence_output = encoder_outputs[0]
