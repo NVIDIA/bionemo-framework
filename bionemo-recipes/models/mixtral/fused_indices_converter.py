7dfbbbde271ca03ba906eca08635ab3b69ffcc0d
Peter St. John <pstjohn@nvidia.com>
2026-03-12 19:08:38 -0600

    dedupe headers
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- bionemo-recipes/models/mixtral/fused_indices_converter.py
+++ bionemo-recipes/models/mixtral/fused_indices_converter.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: LicenseRef-Apache2
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
